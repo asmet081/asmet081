4199adf149563b9924273f47a5a3bbe028a21a5b
asmet081 <91230744+asmet081@users.noreply.github.com>
2023-06-13 20:19:14 -0500

    Add files via upload

---
+++ Past_Projects/CIS340-Advance Tech & Dev/Assign03_Smetana/Assign03/Assign03/Assign03/Employee.cs
@@ -0,0 +1,64 @@
+﻿/*Alex Smetana 
+ * CIS 340 - Assign03
+ * 02/09/2023
+ */
+
+//Employee Class
+//Purpose: Demonstration of creation of Object, Constructor and ToString using C#
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Employee Class
+namespace Assign03
+{
+    internal class Employee
+    {
+
+        //Id, Name, Salary Properties
+        //Getters + Setters included
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int salary;
+
+        public int Salary
+        {
+            get { return salary; }
+            set { if (salary >= 0)
+                {
+                    salary = value;
+                }
+            }
+        }
+
+        #region Constructors
+        //Employee Constructor
+        public Employee(long id, string name, int salary)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Salary = salary;
+        }
+
+        //No Arg Constructor
+        public Employee()
+        {
+            this.Id = 0;
+            this.Name = "Null";
+            this.Salary = 0;
+        }
+        #endregion
+
+        //ToString
+        //Returns the values of ID, Name, and Salary
+        public override string ToString()
+        {
+            StringBuilder msg = new StringBuilder();
+            msg.Append("Employee ID is " + Id + ", ").Append(" Name is " + Name + ", ").Append(" Salary is $" + Salary);
+            return msg.ToString();
+        }
+    }
+}
---
+++ Past_Projects/CIS340-Advance Tech & Dev/Assign03_Smetana/Assign03/Assign03/Assign03/EmployeeDriver.cs
@@ -0,0 +1,39 @@
+﻿/*Alex Smetana 
+ * CIS 340 - Assign03
+ * 02/09/2023
+ */
+
+
+//Main Classs
+//Purpose: Allows for a demonstration of the Employee Class by use of Insantiation
+namespace Assign03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee1 Object Creation - Added attributes and Displays to user
+            Console.WriteLine("Original Employee 1 Information:");
+            Employee employee1 = new Employee();
+            employee1.Id = (1);
+            employee1.Name = ("Garry");
+            employee1.Salary = (90000);
+            Console.WriteLine(employee1.ToString() + "\n");
+
+            //Employee1 Update of Name + Display
+            Console.WriteLine("Updated Employee 1 Information:");
+            employee1.Name = ("Larry");
+            Console.WriteLine(employee1.ToString() + "\n");
+
+            //Employee2 Object Creation - Added attributes and Displays to user
+            Console.WriteLine("Original Employee 2 Information:");
+            Employee employee2 = new Employee(2, "John", 700000);
+            Console.WriteLine(employee2.ToString() + "\n");
+
+            //Employee2 Update of Name + Display
+            Console.WriteLine("Updated Employee 2 Information:");
+            employee2.Salary = 100000;
+            Console.Write(employee2.ToString() + "\n");
+        }
+    }
+}
